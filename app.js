42e16faca90d2a242ac740f8f8d18ec2680f0077
Benoit Caccinolo <bcaccinolo@synbioz.com>
2018-01-17 13:20:34 +0100

    refacto V1

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 const WebSocket = require('ws');
 
 // command line arguments
@@ -8,126 +8,127 @@ process.argv.forEach(function (val, index, array) {
 });
 
 // Connection ports
-ws_server_port = process.argv[2];
-web_server_port = Number(ws_server_port) + 1;
+const ws_server_port = process.argv[2];
+const web_server_port = Number(ws_server_port) + 1;
 
-wss = new WebSocket.Server({port:ws_server_port, clientTracking:true});
-console.log('\n**********************************************************');
-console.log("websocket server listening on port ", ws_server_port);
-let sockets = [];
+// P2P class
+class P2P {
 
-const listenMessage = (message, ws) => {
-console.log('\n**********************************************************');
-console.log('in listen message with data', message);
-
-  json = JSON.parse(message);
-
-  // New Peer Broadcast
-  if (json.type === 'newPeerBroadcast') {
-    // broadcasting to other peers
-    sockets.forEach((socket) => {
-      message = {type: 'newPeer', port: json.port};
-      socket.send(JSON.stringify(message));
-    })
-    ws.send(JSON.stringify({type: 'message', data:'From: '+ ws_server_port +' hello new peer'}));
-    console.log('New peer node added ', json.port);
-    sockets.push(ws);
-
-    message = {type: 'addMe', port: json.port};
-    ws.send(JSON.stringify(message));
+  constructor(port) {
+    this.port = port;
+    this.sockets = [];
   }
 
-  // Add New Peer request
-  if (json.type === 'newPeer') {
+  createServer() {
+    this.server = new WebSocket.Server({port:this.port});
+    console.log("P2P: server listening on port ", this.port);
 
-    url = "ws://localhost:"+json.port;
-    console.log(url);
+    this.server.on('connection', (ws) => {
+      console.log('connection');
+      ws.on('message', (data) => {
+        this.listenMessage(data, ws);
+      })
+    })
+  }
 
-    ws = new WebSocket(url);
-    ws.on('open', function open() {
-      console.log('connection new peer simple');
+  createMessage(json) {
+    return JSON.stringify(json);
+  }
 
-      message = {type: 'addMe', port: json.port};
-      ws.send(JSON.stringify(message));
+  sendMessage(message) {
+    this.sockets.forEach((socket) => {
+      socket.send(this.createMessage({type: 'message', data: message}));
+    })
+  }
 
-      ws.on('message', function(data){
-        console.log(data);
+  addSocket(ws) {
+    console.log('### Adding new socket in the list');
+    ws.send(this.createMessage({type: 'message', data: "From " + ws_server_port + " ok i added you"}));
+    this.sockets.push(ws);
+  }
+
+  listenMessage(message, ws) {
+
+    console.log('\n**********************************************************');
+    console.log('in listen message with data', message);
+
+    let json = JSON.parse(message);
+
+    // New Peer Broadcast
+    if (json.type === 'newPeerBroadcast') {
+
+      this.sockets.forEach((socket) => {
+        socket.send(this.createMessage({type: 'newPeer', port: json.port}));
       })
 
-      sockets.push(ws);
+      console.log('New peer node added ', json.port);
+      this.addSocket(ws);
+
+      ws.send(this.createMessage({type: 'addMe', port: json.port}));
+    }
+
+    // Add New Peer request
+    if (json.type === 'newPeer') {
+
+      let url = "ws://localhost:"+json.port;
+      console.log(url);
+
+      let ws = new WebSocket(url);
+      ws.on('open',  () => {
+        console.log('connection new peer simple');
+        ws.send(this.createMessage({type: 'addMe', port: json.port}));
+        ws.on('message', function(data){
+          console.log(data);
+        })
+        this.addSocket(ws);
+      })
+    }
+
+    // Add the peer
+    if (json.type === 'addMe') {
+      this.addSocket(ws);
+    }
+
+  }
+
+  newConnection(port) {
+    let url = "ws://localhost:" + port;
+    let ws = new WebSocket(url);
+
+    ws.on('open', () => {
+      ws.send(this.createMessage({ type: 'newPeerBroadcast', port: ws_server_port }));
+      ws.on('message', (msg) => {
+        this.listenMessage(msg, ws);
+      })
+    });
+
+    ws.on('close', () => {
+      console.log('connection closed');
     })
   }
-
-  // Add the peer
-  if (json.type === 'addMe') {
-    sockets.push(ws);
-
-    message = {type: 'message', data: "From " + ws_server_port + " ok i added you"};
-    ws.send(JSON.stringify(message));
-  }
-
 }
+// End P2P class
 
-wss.on('connection', function connection(ws) {
-  console.log('connection');
-
-  ws.on('message', function incoming(data) {
-    listenMessage(data, ws);
-  })
-
-})
-
+const p2p = new P2P(ws_server_port);
+p2p.createServer();
 
 app.get('/', function(req, res) {
   res.send('hello');
 })
 
-app.get('/message', (req, res) => {
-  message = req.query.message;
-  console.log("the message is ", message);
-
-  sockets.forEach((socket) => {
-    message = {type: 'message', data: message};
-    socket.send(JSON.stringify(message));
-  })
-
-  res.send('sending message: ' + message);
-
+app.get('/addPeer', function(req, res) {
+  p2p.newConnection(req.query.port);
+  res.send('add peer');
 })
 
 app.get('/listPeers', function(req, res) {
-  console.log(sockets);
-
+  console.log(p2p.sockets);
   res.send('sockets listing');
 })
 
-app.get('/addPeer', function(req, res) {
-
-  url = "ws://localhost:"+req.query.port;
-  console.log(url);
-
-  ws = new WebSocket(url);
-  ws.on('open', function open() {
-
-    message = {
-      type: 'newPeerBroadcast',
-      port: ws_server_port
-    }
-    ws.send(JSON.stringify(message));
-
-    ws.on('message', (msg) => {
-      console.log('message', msg);
-      listenMessage(msg, ws);
-    })
-
-  });
-
-  ws.on('close', () => {
-    console.log('connection closed');
-
-  })
-
-  res.send('add peer');
+app.get('/message', (req, res) => {
+  p2p.sendMessage(req.query.message);
+  res.send('sending message: ' + req.query.message);
 })
 
 app.listen(web_server_port);
